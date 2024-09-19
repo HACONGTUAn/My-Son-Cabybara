faae8019bfaa8fbef5bbf0057aaa17b64eef6543
HACONGTUAn <86510911+HACONGTUAn@users.noreply.github.com>
2024-09-19 11:43:26 +0700

    new scroll view logic

--- Assets/Game/MainGame/Script/HomeUI.cs
+++ Assets/Game/MainGame/Script/HomeUI.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CapybaraMain
 { 
     public class HomeUI : BaseUI
     {
-      public int index = 0;
-    
-      public void OnClickPlayGame()
+        
+        public Button next;
+        public Button back;
+        public ScrollRect scrollSelectionGame;
+        public Button backHome;
+
+        private int currentIndex = 0;
+        private float itemWidth;
+        private GameObject currentGameObject;
+        private void Start()
         {
-            if (! LoadingResources.Instance.keyValuePairs.ContainsKey(index)) {
+            itemWidth = scrollSelectionGame.content.GetChild(0).GetComponent<RectTransform>().rect.width;
+            
+
+            scrollSelectionGame.content.GetComponent<RectTransform>().sizeDelta = new Vector2(itemWidth * 3 + 69.22f * 2, scrollSelectionGame.content.GetComponent<RectTransform>().sizeDelta.y);
+
+
+
+        }
+       public void ScrollLeft()
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                UpdateScrollPosition();
+            }
+        }
+        public void ScrollRight()
+        {
+            if (currentIndex < scrollSelectionGame.content.childCount - 1)
+            {
+                currentIndex++;
+                UpdateScrollPosition();
+            }
+        }
+        void UpdateScrollPosition()
+        {
+        
+            float targetPosition = currentIndex * (itemWidth + 69.22f* 2);
+            scrollSelectionGame.content.anchoredPosition = new Vector2(-(targetPosition ) , scrollSelectionGame.content.anchoredPosition.y);
+        }
+        public void OnClickPlayGame()
+        {
+            if (! LoadingResources.Instance.keyValuePairs.ContainsKey(currentIndex)) {
                 Debug.Log("No key");
                 return;
             }
-            Instantiate(LoadingResources.Instance.keyValuePairs[index]);
+            currentGameObject = Instantiate(LoadingResources.Instance.keyValuePairs[currentIndex]);
             this.gameObject.SetActive(false);
+            backHome.gameObject.SetActive(true);
+        }
+
+        public void BackHome()
+        {
+            Destroy(currentGameObject);
+            this.gameObject.SetActive(true);
+            backHome.gameObject.SetActive(false);
         }
     }
 }
--- Assets/Game/MainGame/Script/Manager.cs
+++ Assets/Game/MainGame/Script/Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace CapybaraMain
@@ -7,34 +8,71 @@ namespace CapybaraMain
 
 public class Manager : TPRLSingleton<Manager>
 {
-        public int heart;
+        public TMP_Text hearText;
+        public TMP_Text teckitText;
         public List<MiniGame> _data = new List<MiniGame>();
 
-        private void LoadingDataInFile()
+        private int heart = 0;
+        private int ticket = 0;
+
+        private void Start()
+        {
+            // heart
+            if (!PlayerPrefs.HasKey("heart"))
+            {
+                PlayerPrefs.SetInt("heart", heart);
+            }
+            else
+            {
+                //hearText.text = PlayerPrefs.GetInt("heart").ToString();
+            }
+            // teckit
+            if (!PlayerPrefs.HasKey("teckit"))
+            {
+                PlayerPrefs.SetInt("teckit", ticket);
+            }
+            else
+            {
+               // teckitText.text = PlayerPrefs.GetInt("teckit").ToString();
+            }
+
+        }
+
+        // write data 
+        //==========================================================================
+        private void WriteDataInFile()
         {
             DataManager.Instance.WriteDataInJson(_data);
         }
-        private void LoadingDataHeart()
+        private void WriteDataHeart()
         {
-
+          
+            PlayerPrefs.SetInt("heart",heart);
+            PlayerPrefs.Save();
         }
-        private void LoadingDataTicket()
+        private void WriteDataTicket()
         {
-
+            PlayerPrefs.SetInt("teckit", ticket);
+            PlayerPrefs.Save();
         }
+        //==========================================================================
 
+        // call method
+        //data in item 
+        //==========================================================================
+        //Set Data
         public void UpdateDataInFile(BaseID currentMiniGame)
         {
-           int index = FindTypeOfObject(currentMiniGame);
+            int index = FindTypeOfObject(currentMiniGame);
+            if (index < 0) {Debug.Log("No find type Object"); return; }
             MiniGame _dataMiniGame = _data[index];
 
             _dataMiniGame.items = currentMiniGame.items;
 
             _data[index] = _dataMiniGame;
-            Debug.Log("du lieu duoc ghi lai : " + _data[index].items[0].quantity);
-            LoadingDataInFile();
+            WriteDataInFile();
         }
-
+        // Get Data
         public List<Item> ReadDataInFile(BaseID currentMiniGame)
         {
             int index = FindTypeOfObject(currentMiniGame);
@@ -53,6 +91,37 @@ public class Manager : TPRLSingleton<Manager>
             }
             return -1;
         }
-}
+        //===================================================================================
+
+        //call data heart
+        //===================================================================================
+        public void SetHeart(int _heart)
+        {
+            heart = _heart;
+            WriteDataHeart();
+        }
+
+        public int GetHeart()
+        {
+            return heart;
+        }
+
+        //===================================================================================
+
+        //call data teckit
+        //===================================================================================
+        public void SetTicket(int _teckit)
+        {
+            ticket = _teckit;
+            WriteDataTicket();
+        }
+
+        public int GetTicket()
+        {
+            return ticket;
+        }
+        //===================================================================================
+
+    }
 
 }
