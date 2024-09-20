06168710752f3ac99c01d7656557b4180bff0a96
HACONGTUAn <86510911+HACONGTUAn@users.noreply.github.com>
2024-09-20 18:05:10 +0700

    Update new

--- Assets/Game/MainGame/Script/ShopUI.cs
+++ Assets/Game/MainGame/Script/ShopUI.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace CapybaraMain
 {
 
 
 public class ShopUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        [SerializeField] private Text _heartUi;
+        [SerializeField] private Text _ticketUi;
+        // Start is called before the first frame update
+   void OnEnable()
+        {
+            _heartUi.text = Manager.Instance.GetHeart().ToString();
+            _ticketUi.text = Manager.Instance.GetTicket().ToString();
+        }
 
     // Update is called once per frame
     void Update()
